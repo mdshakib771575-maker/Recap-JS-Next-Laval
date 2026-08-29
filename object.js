eb530d2804943643d449b3e380562df59d53e2aa
Shakib <mdshakib771575@gmail.com>
2026-08-29 09:31:41 +0600

    recape object Fundamantals

---
+++ object.js
@@ -0,0 +1,17 @@
+const user = {
+    name:"shakib",
+    position:"programer",
+    age:20,
+    address:{
+        upazila:"Fulgazi",
+        Zila:"Feni"
+    },
+    sports:["Cricket","Footboll","Kabadi"]
+
+}
+
+// console.log(user)
+// console.log(user["address"]["Zila"]="Dhaka");
+
+// console.log(user.address["Zila"]="Dhaka");
+console.log(user.sports[0]);
