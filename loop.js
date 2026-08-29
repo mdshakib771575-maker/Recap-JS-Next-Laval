3c0f69dc950f0643370fc79e6f96523d27ecdb62
Shakib <mdshakib771575@gmail.com>
2026-08-29 09:51:48 +0600

    recap loop

---
+++ loop.js
@@ -0,0 +1,30 @@
+// for(let i = 0;i<=10;i++){
+//     console.log(`count : ${i}`)
+// }
+
+// const Fruits = ["Mango", "apple", "Banana"]
+// for (let i = 0; i <= Fruits.length - 1; i++) {
+//     console.log(Fruits[i])
+// }
+
+// let marks = [60, 70, 80, 90, 40]
+// let max = marks[0]
+// for (let i = 1; i <= marks.length - 1; i++) {
+//     if (max < marks[i]) {
+//         max = marks[i]
+//     }
+// }
+// console.log(max)
+let number = [10, 10, 10, 10, 10]
+// let sum = 0
+// for (let i = 0; i <= number.length - 1; i++) {
+//     sum += number[i]
+// }
+// console.log(sum)
+
+let i = 0
+
+while (i<=number.length-1){
+    console.log(number[i])
+    i++
+}
